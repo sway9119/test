53c3751745cb45df32d3194c8c680fc985122888
sway <40759948+sway9119@users.noreply.github.com>
2023-07-16 20:34:04 +0900

    Update automaton.rb

--- ruby/code_wars/automaton.rb
+++ ruby/code_wars/automaton.rb
@@ -4,9 +4,7 @@ class Automaton
   end
   
   def read_commands(commands)
-    # return true if it ends up on the accepted state
     pointer = 'q1'
-    p commands
     commands.each do |command|
       if command === '1'
         if pointer === 'q1'
@@ -35,3 +33,47 @@ Test.assert_equals(my_automaton.read_commands(["1"]), true)
 
 my_automaton = Automaton.new
 Test.assert_equals(my_automaton.read_commands(["1", "0", "0", "1"]), true)
+
+
+# リファクタリング
+class Automaton
+  def initialize
+    @states = {
+      "q1" => { "0" => "q1", "1" => "q2" },
+      "q2" => { "0" => "q3", "1" => "q2" },
+      "q3" => { "0" => "q2", "1" => "q2" }
+    }
+  end
+  
+  def read_commands(commands)
+    state = "q1"
+    
+    commands.each do |command|
+      state = @states[state][command]
+    end
+    
+    state == "q2"
+  end
+end
+
+
+class Automaton
+  def initialize
+    @state = :q1
+  end
+  
+  def read_commands(commands)
+    commands.each do |command|
+      case @state
+      when :q1
+        @state = command == '1' ? :q2 : :q1
+      when :q2
+        @state = command == '1' ? :q2 : :q3
+      when :q3
+        @state = :q2
+      end
+    end
+    
+    @state == :q2
+  end
+end
