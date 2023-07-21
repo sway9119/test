5714aed116669956d5ea884f3b216d5356bbdc38
sway <40759948+sway9119@users.noreply.github.com>
2023-07-21 23:34:53 +0900

    Update how_many_numbers_III.rb

--- ruby/code_wars/how_many_numbers_III.rb
+++ ruby/code_wars/how_many_numbers_III.rb
@@ -1,10 +1,26 @@
-def find_all(n, k)
-  #your code here
-  count = 0
-  min = 0
-  max = 0
+def find_all(sum_number, digit)
+  count = 0 # n桁の数字の個数
+  min = 0 # n桁の数字の最低値
+  max = 0 # n桁の数字の最大値
   
-  
+  # 
+  result = make_array(sum_number, digit)
   result = [count, min, max]
+  return result
+end
+  
+def make_array(sum_number, digit)
+  result = []
+  str = ""
+  digit.times do
+    str << "0"
+  end
+  limit = "1" + str
+  n = limit.chop.to_i
+  limit = limit.to_i
+  while n < limit
+    result << n if n.digits.sum === sum_number
+    n += 1
+  end
   return result
 end
