5ed268066e4c6354e60c5dcc909f333432d9122c
sway <40759948+sway9119@users.noreply.github.com>
2023-07-28 03:46:35 +0900

    Update how_many_numbers_III.rb

--- ruby/code_wars/how_many_numbers_III.rb
+++ ruby/code_wars/how_many_numbers_III.rb
@@ -34,3 +34,17 @@ def make_array(sum_number, digit)
   end
   return result
 end
+
+
+
+# リファクタリング
+def find_all(n, k)
+  p = (1..9).to_a.repeated_combination(k).select { |c| c.inject(:+) == n } 
+  p.empty? ? [] : [p.size, p.min.join.to_i, p.max.join.to_i] 
+end
+
+# repeated_combination: 引数に指定した個数の配列を生成する
+# 1〜9で桁数個の全配列を作成
+# そのうちinject合計がnの数字をselectした配列をpに代入
+# ポイントは全組み合わせの配列を作成するのとイテレータをループするのでは組み込み関数を使うと断然早いということ。
+# パフォーマンスを改善したいのなら組み込み関数がないかを調べる必要がある。
