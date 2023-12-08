dd1018cc42f55512a896c7b9a9a3b458fc590ef6
sway <40759948+sway9119@users.noreply.github.com>
2023-12-09 01:12:55 +0900

    Update 1608.rb

--- ruby/leetcode/1608.rb
+++ ruby/leetcode/1608.rb
@@ -12,8 +12,30 @@ def special_array(nums)
   return -1
 end
 special_array([0,4,3,0,4])
-# ランタイム 81MS
-# メモリ 213.23MB
-
+# Runtime 81MS
+# Memory 213.23MB
 
 # リファクタリング
+def special_array1(nums)
+  length = nums.length
+  array = Array.new(length)
+  length.downto(1) {|i|
+    count = 0
+    nums.map{|num| count += 1 if num >= i}
+    return count if count == i
+  }
+  return -1
+end
+# Runtime 77ms
+# Memory 211.79MB
+
+def special_array2(nums)
+  nums.length.downto(1) {|i|
+    count = 0
+    nums.map{|num| count += 1 if num >= i}
+    return count if count == i
+  }
+  return -1
+end
+# Runtime 58ms
+# Memory 211.74MB
