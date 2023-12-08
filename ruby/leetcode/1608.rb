a3b48a2a9498af3dd600ae8d52b2d768377bbab0
sway <40759948+sway9119@users.noreply.github.com>
2023-12-09 01:14:39 +0900

    Update 1608.rb

--- ruby/leetcode/1608.rb
+++ ruby/leetcode/1608.rb
@@ -1,5 +1,7 @@
 # @param {Integer[]} nums
 # @return {Integer}
+# Runtime 81MS
+# Memory 213.23MB
 def special_array(nums)
   length = nums.length
   array = Array.new(length)
@@ -12,11 +14,11 @@ def special_array(nums)
   return -1
 end
 special_array([0,4,3,0,4])
-# Runtime 81MS
-# Memory 213.23MB
 
 # リファクタリング
-def special_array1(nums)
+# Runtime 77ms
+# Memory 211.79MB
+def special_array(nums)
   length = nums.length
   array = Array.new(length)
   length.downto(1) {|i|
@@ -26,10 +28,10 @@ def special_array1(nums)
   }
   return -1
 end
-# Runtime 77ms
-# Memory 211.79MB
 
-def special_array2(nums)
+# Runtime 58ms
+# Memory 211.74MB
+def special_array(nums)
   nums.length.downto(1) {|i|
     count = 0
     nums.map{|num| count += 1 if num >= i}
@@ -37,5 +39,41 @@ def special_array2(nums)
   }
   return -1
 end
-# Runtime 58ms
-# Memory 211.74MB
+
+# Leetcode: 1608. Special Array With X Elements Greater Than or Equal X.
+# https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+# Runtime: 64 ms, faster than 65.91% of Ruby online submissions for Special Array With X Elements Greater Than or Equal X.
+# Memory Usage: 209.7 MB, less than 77.27% of Ruby online submissions for Special Array With X Elements Greater Than or Equal X.
+# @param {Integer[]} nums
+# @return {Integer}
+def special_array(nums)
+  (0..nums.size).each do |i|
+      return i if i == nums.count{|x| x >= i}
+  end
+  -1
+end
+# Leetcode: 1608. Special Array With X Elements Greater Than or Equal X.
+# https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+# Runtime: 60 ms, faster than 70.45% of Ruby online submissions for Special Array With X Elements Greater Than or Equal X.
+# Memory Usage: 209.7 MB, less than 95.45% of Ruby online submissions for Special Array With X Elements Greater Than or Equal X.
+# @param {Integer[]} nums
+# @return {Integer}
+def special_array(nums)
+  (0..100).each do |i|
+      return i if i == nums.count{|x| x >= i}
+  end
+  -1
+end
+# Leetcode: 1608. Special Array With X Elements Greater Than or Equal X.
+# https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+# Runtime: 48 ms, faster than 90.91% of Ruby online submissions for Special Array With X Elements Greater Than or Equal X.
+# Memory Usage: 209.7 MB, less than 95.45% of Ruby online submissions for Special Array With X Elements Greater Than or Equal X.
+# @param {Integer[]} nums
+# @return {Integer}
+def special_array(nums)
+  nums.sort!
+  (0..100).each do |i|
+      return i if i == nums.count{|x| x >= i}
+  end
+  -1
+end
