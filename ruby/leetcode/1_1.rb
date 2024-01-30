226326e5213f45f5e379a31dfb4d5d251dd1ec75
sway <40759948+sway9119@users.noreply.github.com>
2024-01-30 21:31:57 +0900

    Update 1_1.rb

--- ruby/leetcode/1_1.rb
+++ ruby/leetcode/1_1.rb
@@ -8,9 +8,55 @@ def order_n(nums)
     @count += 1
   end
 end
+
+# n個の配列に対して、n^2回の処理
+def order_n_multi_2(nums)
+  for i in 0...nums.length
+    for j in 0...nums.length
+      @count += 1
+    end
+  end
+end
+
+# n個の配列に対して、n^3回の処理
+def order_n_multi_3(nums)
+  for i in 0...nums.length
+    for j in 0...nums.length
+      for k in 0...nums.length
+        @count += 1
+      end  
+    end
+  end
+end
+
+# n個の配列に対して、2^n回の処理
+def order_2_multi_n(nums)
+  (2**nums.length).times do
+    @count += 1
+  end
+end
+
+# n個の配列に対して、n!回の処理
+def order_n_recurse(nums)
+  # TODO: 再帰処理の実装
+end
+
+def order_log_n(nums)
+  # TODO: 二分探索処理の実装
+end
+
+
+class Test
+  @count = 0
+
   def self.run(nums)
     [
       :order_n,
+      :order_n_multi_2,
+      :order_n_multi_3,
+      :order_2_multi_n,
+      :order_n_recurse,
+      :order_log_n
     ].each do |method|
       Timeout.timeout(1) do
         result = Benchmark.ms do
@@ -36,3 +82,8 @@ end
 n = 25
 nums = (1..n).to_a
 Test.run(nums)
+
+# n=15; n!が使えなくなる
+# n=25: 2^nが使えなくなる
+# n=1,000: n^3が使えなくなる
+# n=100,000: n^2が使えなくなる
