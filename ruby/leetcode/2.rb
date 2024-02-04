a1ea8bd7f6ec8be709390290d8813abfb246ec01
sway <40759948+sway9119@users.noreply.github.com>
2024-02-04 22:55:36 +0900

    Update 2.rb

--- ruby/leetcode/2.rb
+++ ruby/leetcode/2.rb
@@ -49,3 +49,16 @@ l2 = ListNode.new(5, l2_second_node)
 p add_two_numbers(l1,l2)
 
 
+
+
+# リファクタリング
+# def add_two_numbers(l1, l2)
+#   add_two_numbers_with_overflow(l1, l2, false)
+# end
+
+# def add_two_numbers_with_overflow(l1, l2, overflow)
+#   return unless l1 || l2 || overflow
+#   val = (l1&.val || 0) + (l2&.val || 0) + (overflow ? 1 : 0)
+#   val -= 10 if (overflow = val >= 10)
+#   ListNode.new(val, add_two_numbers_with_overflow(l1&.next, l2&.next, overflow))
+# end
