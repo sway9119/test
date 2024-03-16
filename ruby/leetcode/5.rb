af52502b71e469eac8887e4f4987c5d75960fa34
sway <40759948+sway9119@users.noreply.github.com>
2024-03-16 23:29:40 +0900

    Create 5.rb

---
+++ ruby/leetcode/5.rb
@@ -0,0 +1,12 @@
+# @param {String} s
+# @return {String}
+def longest_palindrome(s)
+  # 文字に分割する
+  s.chars.each_with_index {|c, i|
+      # 逆から再度出現したら文字列とする
+      # 文字列として格納
+      # 2番目と-2番目の文字が一致するか
+      # 中央値以下までループ
+    # 次の文字を検索
+  }
+end
