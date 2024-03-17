bbf4351ace5b8475d859534ff5e0993b9aa18f06
sway <40759948+sway9119@users.noreply.github.com>
2024-03-17 23:28:31 +0900

    Update 5.rb

--- ruby/leetcode/5.rb
+++ ruby/leetcode/5.rb
@@ -1,12 +1,32 @@
 # @param {String} s
 # @return {String}
 def longest_palindrome(s)
-  # 文字に分割する
-  s.chars.each_with_index {|c, i|
-      # 逆から再度出現したら文字列とする
-      # 文字列として格納
-      # 2番目と-2番目の文字が一致するか
-      # 中央値以下までループ
-    # 次の文字を検索
-  }
+  return "" if s.empty?
+
+  start = 0
+  max_length = 1
+
+  (0...s.length).each do |i|
+    # 奇数長の回文の中心を基準に拡張する
+    length1 = expand_around_center(s, i, i)
+    # 偶数長の回文の中心を基準に拡張する
+    length2 = expand_around_center(s, i, i + 1)
+    
+    length = [length1, length2].max
+    if length > max_length
+      start = i - (length - 1) / 2
+      max_length = length
+    end
+  end
+
+  s[start...(start + max_length)]
 end
+
+def expand_around_center(s, left, right)
+  while left >= 0 && right < s.length && s[left] == s[right]
+    left -= 1
+    right += 1
+  end
+  right - left - 1
+end
+
